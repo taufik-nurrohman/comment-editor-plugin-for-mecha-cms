f77d2f56f8edeacdd558472d5f2b24ce35986af8
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-06-25 21:14:18 +0700

    LAUNCH!

---
+++ comment-editor/launch.php
@@ -0,0 +1,17 @@
+<?php
+
+Weapon::add('shell_after', function() {
+    if(Config::get('page_type') == 'article') {
+        echo Asset::stylesheet('manager/shell/editor.css');
+    }
+});
+
+Weapon::add('sword_after', function() {
+    if(Config::get('page_type') == 'article') {
+        echo Asset::script(array(
+            'manager/sword/editor/editor.min.js',
+            'manager/sword/editor/mte.min.js'
+        ));
+        echo '<script>(function(a,b){var c=b.getElementById(\'comment-form\').getElementsByTagName(\'textarea\')[0];new MTE(c,{shortcut:1,toolbarClass:\'editor-toolbar cf\'})})(window,document);</script>';
+    }
+});
