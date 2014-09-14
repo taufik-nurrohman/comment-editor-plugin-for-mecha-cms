a2b71fe480fa6cb53d9609ced99fee8b3f6cbfe1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-09-14 20:04:44 +0700

    For Mecha 1.1.1

--- comment-editor/launch.php
+++ comment-editor/launch.php
@@ -1,8 +1,10 @@
 <?php
 
-Weapon::add('shell_after', function() {
+Weapon::add('shell_after', function() use($config) {
     if(Config::get('page_type') == 'article') {
         echo Asset::stylesheet('manager/shell/editor.css');
+        $font = $config->protocol . 'maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css';
+        if( ! Asset::loaded($font)) echo Asset::stylesheet($font);
     }
 });
 
@@ -12,6 +14,6 @@ Weapon::add('sword_after', function() {
             'manager/sword/editor/editor.min.js',
             'manager/sword/editor/mte.min.js'
         ));
-        echo '<script>(function(a,b){var c=b.getElementById(\'comment-form\').getElementsByTagName(\'textarea\')[0];new MTE(c,{shortcut:1,toolbarClass:\'editor-toolbar cf\'})})(window,document);</script>';
+        echo '<script>(function(a,b){var c=b.getElementById(\'comment-form\').getElementsByTagName(\'textarea\')[0];c.className=c.className+= \' code\';new MTE(c,{shortcut:1,toolbarClass:\'editor-toolbar cf\'})})(window,document);</script>';
     }
 });
