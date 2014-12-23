68f9afbd2177ac3e04805a85d4937f559b203354
Taufik Nurrohman <t.nurrohman77@gmail.com>
2014-12-23 18:45:46 +0700

    For Mecha 1.1.2

--- comment-editor/launch.php
+++ comment-editor/launch.php
@@ -4,19 +4,16 @@ if($config->comments) {
 
     Weapon::add('shell_after', function() use($config) {
         if(Config::get('page_type') == 'article') {
-            echo Asset::stylesheet('manager/shell/editor.css');
-            $font = $config->protocol . 'maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css';
-            if( ! Asset::loaded($font)) echo Asset::stylesheet($font);
+            if( ! Asset::loaded('manager/shell/editor.css')) echo Asset::stylesheet('manager/shell/editor.css');
+            if( ! Asset::loaded($config->protocol . ICON_LIBRARY_PATH)) echo Asset::stylesheet($config->protocol . ICON_LIBRARY_PATH);
         }
     });
 
     Weapon::add('SHIPMENT_REGION_BOTTOM', function() {
         if(Config::get('page_type') == 'article') {
-            echo Asset::javascript(array(
-                'manager/sword/editor/editor.min.js',
-                'manager/sword/editor/mte.min.js'
-            ));
-            echo '<script>(function(a,b){var c=b.getElementById(\'comment-form\').getElementsByTagName(\'textarea\')[0];c.className=c.className+= \' code\';new MTE(c,{shortcut:1,toolbarClass:\'editor-toolbar cf\'})})(window,document);</script>';
+			if( ! Asset::loaded('manager/sword/editor/editor.min.js')) echo Asset::javascript('manager/sword/editor/editor.min.js');
+			if( ! Asset::loaded('manager/sword/editor/mte.min.js')) echo Asset::javascript('manager/sword/editor/mte.min.js');
+            echo O_BEGIN . '<script>(function(a,b){var c=b.getElementById(\'comment-form\').getElementsByTagName(\'textarea\')[0];c.className=c.className+= \' code\';new MTE(c,{shortcut:1,toolbarClass:\'editor-toolbar cf\'})})(window,document);</script>' . O_END;
         }
     });
 
