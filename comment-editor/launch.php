2325b09ca91bbfd9fcf63b579f33da211dcb59ef
Taufik Nurrohman <t.nurrohman77@gmail.com>
2015-02-05 12:36:41 +0700

    Updates

--- comment-editor/launch.php
+++ comment-editor/launch.php
@@ -9,11 +9,11 @@ if($config->comments) {
         }
     });
 
-    Weapon::add('SHIPMENT_REGION_BOTTOM', function() {
+    Weapon::add('SHIPMENT_REGION_BOTTOM', function() use($config) {
         if(Config::get('page_type') == 'article') {
-			if( ! Asset::loaded('manager/sword/editor/editor.min.js')) echo Asset::javascript('manager/sword/editor/editor.min.js');
-			if( ! Asset::loaded('manager/sword/editor/mte.min.js')) echo Asset::javascript('manager/sword/editor/mte.min.js');
-            echo O_BEGIN . '<script>(function(a,b){var c=b.getElementById(\'comment-form\').getElementsByTagName(\'textarea\')[0];c.className=c.className+= \' code\';new MTE(c,{shortcut:1,toolbarClass:\'editor-toolbar cf\'})})(window,document);</script>' . O_END;
+            if( ! Asset::loaded('manager/sword/editor/editor.min.js')) echo Asset::javascript('manager/sword/editor/editor.min.js');
+            if( ! Asset::loaded('manager/sword/editor/mte.min.js')) echo Asset::javascript('manager/sword/editor/mte.min.js');
+            echo O_BEGIN . '<script>(function(a,b){if(typeof MTE=="undefined"){return}var c=b.getElementById(\'comment-form\');if(!c){return}var t=c.message,p=c.content_type;if(typeof HTE!="undefined"){var k=\'' . $config->html_parser . '\';if(k==\'HTML\'){MTE=HTE;if(!p){var o=b.createElement(\'input\');o.name=\'content_type\';o.type=\'hidden\';o.value=\'HTML\';c.appendChild(o)}}}t.className+=\' code\';new MTE(t,{shortcut:1,toolbarClass:\'editor-toolbar cf\',buttonClassPrefix:\'editor-toolbar-button editor-toolbar-button-\',iconClassPrefix:\'fa fa-\',emptyElementSuffix:\'' . ES . '\'})})(window,document);</script>' . O_END;
         }
     });
 
